4384da5434e33cc6dc2e6e5ca339185ad4fd5c46
killthebee <superhotfire447@yandex.ru>
2022-10-25 23:03:57 +0400

    init balance and event pop-up buttons layout

--- BillChopper/Controller/Main.swift
+++ BillChopper/Controller/Main.swift
@@ -8,21 +8,62 @@ class Main: UIViewController {
     var viewContext: NSManagedObjectContext!
     let testData: [String] = ["1", "2", "3", "4" , "5", "6", "7", "8" , "5", "6", "7", "8" , "5", "6", "7", "8" , "5", "6", "7", "8" , "5", "6", "7", "8" , "5", "6", "7", "8"]
     
+    let screenRect = UIScreen.main.bounds
+    let tenthOfWindowHeight = UIScreen.main.bounds.size.height / 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         viewContext = viewController.container.viewContext
         let tableView = setUpTableView()
+        let eventButton = setUpEventButton()
+        let balanceButton = setUpBalanceButton()
+        
         
         view.addSubview(tableView)
+        view.addSubview(eventButton)
+        view.addSubview(balanceButton)
     }
     
     override func loadView() {
         super.loadView()
     }
     
+    private func setUpEventButton() -> UIButton {
+        let frame = CGRect(
+            x: tenthOfWindowHeight * 0.3,
+            y: screenRect.size.width * 0.3,
+            width: screenRect.size.width * 0.35,
+            height: tenthOfWindowHeight * 0.4
+        )
+        let eventButton = UIButton(frame: frame)
+        
+        eventButton.backgroundColor = UIColor.systemGray
+        eventButton.layer.cornerRadius = 15
+        eventButton.clipsToBounds = true
+        eventButton.titleLabel?.text = "tak bleat"
+        
+        return eventButton
+    }
+    
+    private func setUpBalanceButton() -> UIButton{
+        let frame = CGRect(
+            x: tenthOfWindowHeight * 2.1,
+            y: screenRect.size.width * 0.3,
+            width: screenRect.size.width * 0.35,
+            height: tenthOfWindowHeight * 0.4
+        )
+        
+        let balanceButton = UIButton(frame: frame)
+        
+        balanceButton.backgroundColor = UIColor.systemGreen
+        balanceButton.layer.cornerRadius = 15
+        balanceButton.clipsToBounds = true
+        balanceButton.titleLabel?.text = "tak bleat"
+        
+        return balanceButton
+    }
+    
     private func setUpTableView() -> UITableView {
-        let screenRect = UIScreen.main.bounds
-        let tenthOfWindowHeight = screenRect.size.height / 10
         let frameY = 2 * tenthOfWindowHeight
         let frameX = screenRect.size.width * 0.05
         let frameWidth = screenRect.size.width - screenRect.size.width * 0.1
@@ -83,11 +124,11 @@ extension Main: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section != 3 {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: SpendTableViewCell.identifier) as? SpendTableViewCell else { return UITableViewCell() }
+
             return cell 
         }
         else {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: LentCell.newIdentifier) as? LentCell else { return UITableViewCell() }
-            print("here here")
             return cell
         }
     }
