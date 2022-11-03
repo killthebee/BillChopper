0b980180f9907fd8b982bb1c87d2197ad47def04
killthebee <superhotfire447@yandex.ru>
2022-11-03 13:12:30 +0300

    add total balance and events to footer

--- BillChopper/View/FooterView.swift
+++ BillChopper/View/FooterView.swift
@@ -1,6 +1,12 @@
 import UIKit
 
 
+enum balanceType: String {
+    case borrow = "you borrowed"
+    case owe = "you lent"
+}
+
+
 final class FooterView: UIView {
     
     // TODO: Hide it somewhere else
@@ -15,8 +21,11 @@ final class FooterView: UIView {
         addLayer()
         
         let plusIconView = setupPlusIconView()
-        //let balanceView = setupBalanceView()
+        let balanceView = setupBalanceView()
+        let eventsView = setupEventsView()
         self.addSubview(plusIconView)
+        self.addSubview(balanceView)
+        self.addSubview(eventsView)
     }
     
     required init?(coder: NSCoder) {
@@ -66,4 +75,110 @@ final class FooterView: UIView {
         
         return iconView
     }
+    
+    private func setupBalanceView() -> UIView {
+        let balanceView = UIView()
+        let balanceWithLabal = UILabel()
+        let balanceTypeLabel = UILabel()
+        let balance = UILabel()
+        
+        
+        balanceView.addSubview(balanceWithLabal)
+        balanceView.addSubview(balanceTypeLabel)
+        balanceView.addSubview(balance)
+        // TODO: make the aplication of color to labels as external func
+        
+//        balanceView.layer.borderColor = UIColor.black.cgColor
+//        balanceView.layer.borderWidth = 1
+        
+        
+        // TODO: format string
+        balanceWithLabal.text = "in total:"
+        balanceWithLabal.textAlignment = .right
+        
+        balanceTypeLabel.text = "you borrowed"
+        balanceTypeLabel.font = balanceTypeLabel.font.withSize(11)
+        balanceTypeLabel.textColor = .red
+        balanceTypeLabel.textAlignment = .right
+        
+        balance.text = "3222 usd"
+        balance.font = balance.font.withSize(23)
+        balance.textColor = .red
+        balance.textAlignment = .right
+        
+        balanceView.frame = CGRect(
+            x: tenthOfWidnowWidth * 6,
+            y: tenthOfWindowHeight * 8.7,
+            width: tenthOfWidnowWidth * 3.3,
+            height: tenthOfWidnowWidth * 2
+        )
+        
+        balanceWithLabal.frame = CGRect(
+            x: 0,
+            y: tenthOfWidnowWidth * 0.2,
+            width: tenthOfWidnowWidth * 3,
+            height: tenthOfWidnowWidth * 0.5
+        )
+        
+        balanceTypeLabel.frame = CGRect(
+            x: 0,
+            y: tenthOfWidnowWidth * 0.7,
+            width: tenthOfWidnowWidth * 3,
+            height: tenthOfWidnowWidth * 0.5
+        )
+        
+        balance.frame = CGRect(
+            x: 0,
+            y: tenthOfWidnowWidth * 1.2,
+            width: tenthOfWidnowWidth * 3,
+            height: tenthOfWidnowWidth * 0.5
+        )
+        
+        //balanceWithLabal.superview?.bringSubviewToFront(balanceWithLabal)
+        return balanceView
+    }
+    
+    private func setupEventsView() -> UIView {
+        let eventsView = UIView()
+        let eventName = UILabel()
+        let eventLable = UILabel()
+        
+        eventsView.addSubview(eventName)
+        eventsView.addSubview(eventLable)
+        
+//        eventsView.layer.borderColor = UIColor.black.cgColor
+//        eventsView.layer.borderWidth = 1
+        
+        eventName.text = "ALL"
+        eventName.font = UIFont.boldSystemFont(ofSize: 25)
+        eventName.textAlignment = .center
+        eventName.textColor = .red
+        
+        eventLable.text = "events"
+        eventLable.textAlignment = .center
+        
+        eventsView.frame = CGRect(
+            x: tenthOfWidnowWidth * 0.6,
+            y: tenthOfWindowHeight * 8.7,
+            width: tenthOfWidnowWidth * 3.3,
+            height: tenthOfWidnowWidth * 2
+        )
+        
+        eventName.frame = CGRect(
+            x: 0,
+            y: tenthOfWidnowWidth * 0.5,
+            width: tenthOfWidnowWidth * 3.3,
+            height: tenthOfWidnowWidth * 0.5
+        )
+        
+        eventLable.frame = CGRect(
+            x: 0,
+            y: tenthOfWidnowWidth * 1.2,
+            width: tenthOfWidnowWidth * 3.3,
+            height: tenthOfWidnowWidth * 0.3
+        )
+        
+        return eventsView
+    }
+    
 }
