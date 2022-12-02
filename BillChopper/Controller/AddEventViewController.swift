49b37655f9b15dc8d8081ea47d690e215cf296d1
killthebee <superhotfire447@yandex.ru>
2022-12-02 11:16:44 +0300

    add icon and name text to event

--- BillChopper/Controller/AddEventViewController.swift
+++ BillChopper/Controller/AddEventViewController.swift
@@ -1,17 +1,84 @@
-//
-//  AddEventViewController.swift
-//  BillChopper
-//
-//  Created by danny on 28.11.2022.
-//
-
 import UIKit
 
+
 class AddEventViewController: UIViewController {
+    
+    lazy var iconView: ProfileIcon = setUpIconView()
+    lazy var eventNameTextField: UITextField = setUpUsernameTextField()
+    lazy var eventNameHelpLable: UILabel = setUpEventNameHelp()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .white
+        view.addSubview(iconView)
+        view.addSubview(eventNameTextField)
+        view.addSubview(eventNameHelpLable)
     }
+    
+    // I can refactor the shit out of this
+    private func setUpIconView(_ image: UIImage = UIImage(named: "eventIcon")!) -> ProfileIcon {
+        let profileIcon = ProfileIcon()
+        profileIcon.image = image
+        // for the fuck sake stop using width to messure height...
+        profileIcon.frame = CGRect(
+            x: view.frame.size.width * 0.1,
+            y: view.frame.size.height * 0.05,
+            width: view.frame.size.width * 0.3,
+            height: view.frame.size.width * 0.3
+        )
+        
+//        let tapOnIconGestureRecognizer = UITapGestureRecognizer(
+//            target: self, action: #selector(handleTapOnIcon)
+//        )
+//        profileIcon.isUserInteractionEnabled = true
+//        profileIcon.addGestureRecognizer(tapOnIconGestureRecognizer)
+        
+        return profileIcon
+    }
+    
+    private func setUpUsernameTextField() -> UITextField {
+        let eventNameTextField = CustomTextField(frame: CGRect(
+            x: view.frame.size.width * 0.45,
+            y: view.frame.size.height * 0.1,
+            width: view.frame.size.width * 0.5,
+            height: view.frame.size.height * 0.05
+        ))
+        //eventNameTextField.text = "new event name"
+        eventNameTextField.placeholder = "new event name"
+        eventNameTextField.font = UIFont.boldSystemFont(ofSize: 21)
+        eventNameTextField.autocorrectionType = UITextAutocorrectionType.no
+        eventNameTextField.clearButtonMode = UITextField.ViewMode.whileEditing
+        eventNameTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        eventNameTextField.keyboardType = UIKeyboardType.default
+        eventNameTextField.returnKeyType = UIReturnKeyType.done
+        eventNameTextField.delegate = self
+        
+        eventNameTextField.sidePadding = eventNameTextField.frame.width * 0.05
+        eventNameTextField.topPadding = eventNameTextField.frame.height * 0.1
+        eventNameTextField.layer.borderColor = UIColor.black.cgColor
+        eventNameTextField.layer.borderWidth = 1
+        eventNameTextField.layer.cornerRadius = 15
+        eventNameTextField.backgroundColor = UIColor(
+            hue: 0/360, saturation: 0/100, brightness: 98/100, alpha: 1.0
+        )
+        
+        return eventNameTextField
+    }
+    
+    private func setUpEventNameHelp() -> UILabel {
+        let eventNameHelpLable = UILabel()
+        eventNameHelpLable.frame = CGRect(
+            x: view.frame.size.width * 0.47,
+            y: view.frame.size.height * 0.14,
+            width: view.frame.size.width * 0.5,
+            height: view.frame.size.height * 0.05
+        )
+        eventNameHelpLable.text = "Name the event"
+        return eventNameHelpLable
+    }
+}
+
+
+extension AddEventViewController: UITextFieldDelegate {
     
 }
