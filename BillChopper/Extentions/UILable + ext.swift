5f1b6ebac89b3bb7acdd40e27c9e608ff9d9b4c3
killthebee <superhotfire447@yandex.ru>
2023-01-07 16:15:55 +0300

    add color to custom init

--- BillChopper/Extentions/UILable + ext.swift
+++ BillChopper/Extentions/UILable + ext.swift
@@ -2,8 +2,11 @@ import UIKit
 
 
 extension UILabel {
-    convenience init(frame: CGRect = .zero, text: String) {
+    
+    convenience init(frame: CGRect = .zero, text: String, color: UIColor = .black) {
         self.init(frame: frame)
         self.text = text
+        self.textColor = color
     }
+    
 }
