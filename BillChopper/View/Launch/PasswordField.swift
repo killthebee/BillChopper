57e68ba40478ca208ef893bdf8755f02280af7e9
killthebee <superhotfire447@yandex.ru>
2023-03-02 12:23:02 +0300

    add custom icons for pw field in launch screen

--- BillChopper/View/Launch/PasswordField.swift
+++ BillChopper/View/Launch/PasswordField.swift
@@ -3,13 +3,10 @@ import UIKit
 class PasswordField: UITextField {
     
     private let visibilityButton: UIButton = {
-        let eyeImage = UIImage(systemName: "eye")
-        
-        
-        let button = UIButton(type: .custom)
-        button.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
-        button.setImage(eyeImage, for: .normal)
-        button.setImage(UIImage(named: "ic_visibility_on"), for: .selected)
+        let button = UIButton()
+        button.setImage(R.image.pwHidden(), for: .normal)
+        button.setImage(R.image.pwShown(), for: .selected)
+        button.imageView?.contentMode = .scaleAspectFit
         button.addTarget(self, action: #selector(showHidePassword(_:)), for: .touchUpInside)
         return button
     }()
@@ -25,10 +22,15 @@ class PasswordField: UITextField {
     
     private func setup() {
         self.isSecureTextEntry = true
+        visibilityButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
         rightView = visibilityButton
         rightViewMode = .always
         
         self.returnKeyType = UIReturnKeyType.done
+    }
+    
+    override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
+        return .init(x:bounds.width - 29, y: 5, width: 30, height: 30)
     }
     
     @objc private func showHidePassword(_ sender: UIButton) {
