bad9100786aa94695d47b10d17c7434ad6cb31d2
killthebee <danny.developer@ro.ru>
2023-05-15 17:04:17 +0300

    add image downloading at profile vc

--- BillChopper/Controller/ProfileViewController.swift
+++ BillChopper/Controller/ProfileViewController.swift
@@ -289,21 +289,39 @@ final class ProfileViewController: UIViewController {
     
     @objc func handleSaveButtonClicked(_ sender: UIButton) {
         print("save requested")
+        // upload image
 //        let filename = "\(usernameTextField.text ?? "anon").png"
 //        uploadImage(fileName: filename, image: iconView.image!)
-        
-        let (isValid, validationResult) = Verifier().verifyUserUpdate(
-            username: self.usernameTextField.text,
-            gender: self.PhoneAndGender.genderButton.titleLabel?.text,
-            phone: (self.PhoneAndGender.phoneInput.text ?? "") + (self.PhoneAndGender.codeInput.text ?? "")
-        )
-        if !isValid {
-            setWarnings(erros: validationResult)
-            return
+        // upload profile updates
+//        let (isValid, validationResult) = Verifier().verifyUserUpdate(
+//            username: self.usernameTextField.text,
+//            gender: self.PhoneAndGender.genderButton.titleLabel?.text,
+//            phone: (self.PhoneAndGender.phoneInput.text ?? "") + (self.PhoneAndGender.codeInput.text ?? "")
+//        )
+//        if !isValid {
+//            setWarnings(erros: validationResult)
+//            return
+//        }
+//        let jsonData = try? JSONSerialization.data(withJSONObject: validationResult)
+//        let request = setupRequest(url: .updateUser, method: .put)
+        // download image
+        let setupImage = { [unowned self] (imageData: Data) in
+            DispatchQueue.main.async {
+                self.iconView.removeFromSuperview()
+                self.iconView = ProfileIcon().setUpIconView(UIImage(data: imageData)!)
+                self.setupIconView(iconView: self.iconView)
+                
+                let tapOnIconGestureRecognizer = UITapGestureRecognizer(
+                    target: self, action: #selector(self.handleTapOnIcon)
+                )
+                self.iconView.isUserInteractionEnabled = true
+                self.iconView.addGestureRecognizer(tapOnIconGestureRecognizer)
+                self.view.addSubview(self.iconView)
+            }
         }
-        let jsonData = try? JSONSerialization.data(withJSONObject: validationResult)
-        let request = setupRequest(url: .updateUser, method: .put)
-        
+        let imageUrl = URL(string: "http://127.0.0.1:8000/media/images/user/2/20230515063803897.png")!
+        let request = URLRequest(url: imageUrl)
+        performRequest(request: request, successHandler: setupImage)
     }
     
     @objc func handleExitButtonClicked(_ sender: UIButton) {
